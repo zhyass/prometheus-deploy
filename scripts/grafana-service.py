4f3f8b26c27938407e4b9c86215f87cfb5a93fcb
zhyass <34016424+zhyass@users.noreply.github.com>
2018-08-14 19:03:06 +0800

    fix: fix some bugs

--- scripts/grafana-service.py
+++ scripts/grafana-service.py
@@ -4,6 +4,8 @@ import json
 import yaml
 import sys
 import os
+import subprocess
+import time
 import traceback
 import logging.handlers
 import sqlite3
@@ -12,9 +14,15 @@ import requests
 SUPPORTED_ACTIONS = {
     "displayjobs": "display the prometheus monitor job list",
     "addjob": "add job to the prometheus monitor job list",
+    "deljob": "delete job from the prometheus monitor job list",
     "reset": "reset the grafana server's admin password to default(admin)",
     "modifyuser": "modify the admin's password",
-    "dashboards": "display dashboard for download"
+    "dashboards": "display dashboard for download",
+    "healthcheck": "check the service status",
+    "healthaction": "take action if service check health failed",
+    "start": "start prometheus and grafana-server",
+    "stop": "stop prometheus and grafana-server",
+    "restart": "restart prometheus and grafana-server"
 }
 
 # call the function before calling other functions of the current file
@@ -39,34 +47,83 @@ def get_json_params(params=None):
     return json_params
 
 def add_job(job_cnf):
-    logger.info("add_job_prometheus_job_list")
+    logger.info("add_job")
 
     if not job_cnf:
         logger.error("add_job without config")
         return -1
 
     job_name = job_cnf.get("job_name", "").strip()
-    targets = job_cnf.get("targets", "").strip()
+    ip = job_cnf.get("ip", "").strip()
+    port = job_cnf.get("port", "")
     if not job_name:
         logger.error("add_job get job_name [%s] failed" % job_name)
         return -1
-    if not targets:
-        logger.error("add_job get targets [%s] failed" % targets)
+    if not ip:
+        logger.error("add_job get ip [%s] failed" % ip)
+        return -1
+    if not port:
+        logger.error("add_job get port [%d] failed" % port)
         return -1
 
     fd = open('/opt/prometheus/prometheus.yml', 'a')
     l = []
     l.append("  - job_name: '" + job_name + "'\n")
     l.append("    static_configs:\n")
-    l.append("    - targets: ['" + targets + "']\n")
+    l.append("    - targets: ['" + ip + ":" + str(port) + "']\n")
     fd.writelines(l)
     fd.close()
+
+    ret_code, _ = exec_cmd('service prometheus restart')
+    if ret_code != 0:
+        logger.error('add_job restart service prometheus failed')
+        return -1
 
     logger.info("add_job succeeded")
     return 0
 
+def del_job(job_cnf):
+    logger.info("del_job")
+    
+    if not job_cnf:
+        logger.error("del_job without config")
+        return -1
+
+    job_name = job_cnf.get("job_name","").strip()
+    if not job_name:
+        logger.error("del_job get job_name [%s] failed" % job_name)
+        return -1
+
+    iflag = 0
+    s = "- job_name: '" + job_name + "'"
+    with open('/opt/prometheus/prometheus.yml', 'r') as f:
+        lines = f.readlines()
+    f.close()
+    with open('/opt/prometheus/prometheus.yml', 'w') as g:
+        for line in lines:
+            if iflag == 0:
+                if s in line:
+                    iflag = 1
+                    continue
+                g.write(line)
+            elif iflag == 1:
+                if '- job_name: ' in line:
+                    iflag = 2
+                    g.write(line)
+            else :
+                g.write(line)
+    g.close()
+
+    ret_code, _ = exec_cmd('service prometheus restart')
+    if ret_code != 0:
+        logger.error('del_job restart service prometheus failed')
+        return -1
+
+    logger.info("del_job succeeded")
+    return 0
+
 def modify_user(user_cnf):
-    logger.info("modify_grafana_user_password")
+    logger.info("modify_user")
 
     if not user_cnf:
         logger.error("modify_user without config")
@@ -85,7 +142,7 @@ def modify_user(user_cnf):
         logger.error("modify_user get confirmnew [%s] failed" % confirmnew)
         return -1
     if newpasswd != confirmnew:
-        logger.error("please makesure the password is same!")
+        logger.error("modify_user please makesure the password is same!")
         return -1
 
     try:
@@ -95,23 +152,23 @@ def modify_user(user_cnf):
             "newPassword": newpasswd,
             "confirmNew": confirmnew
         }
-        res = requests.put(url, json=body, headers={"Content-Type": "application/json"}, auth=('admin', 'admin'))
+        res = requests.put(url, json=body, headers={"Content-Type": "application/json"}, auth=('admin', oldpasswd))
         if res.status_code != 200:
-            logger.error("http request failed, status [%d], reason [%s], data [%s]"
+            logger.error("modify_user failed, status [%d], reason [%s], data [%s]"
                          % (res.status_code, res.reason, res.text))
             return -1
 
-        logger.info("http request succeeded, status [%d], reason [%s], data [%s]"
+        logger.info("modify_user succeeded, status [%d], reason [%s], data [%s]"
                      % (res.status_code, res.reason, res.text))
         return 0
     except:
         err_msg = traceback.format_exc()
         logger.error(err_msg)
-        logger.error("http_request failed")
+        logger.error("modify_user failed")
         return -1
 
 def reset_admin():
-    logger.info("reset_grafana_admin_password")
+    logger.info("reset_admin")
 
     conn = sqlite3.connect("/var/lib/grafana/grafana.db")
     conn.execute("update user set password = '59acf18b94d7eb0694c61e60ce44c110c7a683ac6a8f09580d626f90f4a242000746579358d77dd9e570e83fa24faa88a8a6', salt = 'F3FAxVm33R' where login = 'admin'")
@@ -122,7 +179,7 @@ def reset_admin():
     return 0
 
 def disp_list():
-    logger.info("display_prometheus_job_list")
+    logger.info("disp_list")
 
     fd = open('/opt/prometheus/prometheus.yml', 'r')
     dict_tmp = yaml.load(fd)
@@ -133,33 +190,158 @@ def disp_list():
         job_name = i['job_name']
         for j in i['static_configs']:
             for k in j['targets']:
-                l.append([job_name,k])
+                lst = k.split(":")
+                if len(lst) != 2:
+                    logger.error("disp_list failed, unsyntax err for 'prometheus.yml'")
+                    return -1
+                l.append([job_name, lst[0], lst[1]])
 
     ret_cnf = {
-        "labels": ["job_name", "targets"],
+        "labels": ["job_name", "ip", "port"],
         "data": l
     }
     print json.dumps(ret_cnf)
 
-    logger.info("display_prometheus_job_list succeeded")
+    logger.info("disp_list succeeded")
     return 0
 
 def disp_dashboard():
-    logger.info("display_dashboard_for_download")
+    logger.info("disp_dashboard")
 
     l = []
     ftppath = '/srv/ftp'
     fileList = os.listdir(ftppath)
     for fileName in fileList:
         if 'json' in fileName:
-            l.append(fileName)
+            l.append([fileName])
     ret_cnf = {
         "labels": ["fileName"],
         "data": l
     }
     print json.dumps(ret_cnf)
     
-    logger.info("display_dashboard_for_download succeeded")
+    logger.info("disp_dashboard succeeded")
+    return 0
+
+def exec_cmd(cmd):
+    """
+    :param cmd: the command you want to call
+    :return: ret_code, output
+    """
+    try:
+        ret = subprocess.check_output(cmd, shell=True)
+        return 0, ret
+    except subprocess.CalledProcessError as e:
+        return e.returncode, e.output
+
+def check_local_service(service,port):
+    cmd = 'nc -z -v -w10 127.0.0.1 %d' % port
+    ret_code, _ = exec_cmd(cmd)
+    if ret_code != 0:
+        logger.error('no process listen at %d' % port)
+        return False
+
+    cmd = 'pidof %s' % service
+    ret_code, output = exec_cmd(cmd)
+    if ret_code != 0 or len(output) == 0:
+        #port occupied by other process,need kill the process 
+        command='''kill -9 $(netstat -nlp | grep : %d | awk '{print $7}' | awk -F"/" '{ print $1 }')''' % port
+        exec_cmd(command)
+        logger.error('cannot find %s process' % service)
+        return False
+    return True
+
+def health_check():
+    logger.info("health_check")
+    bflag = False
+    if not check_local_service("prometheus", 9090):
+        logger.error('check prometheus process failed')
+        bflag = True
+
+    if not check_local_service("grafana-server", 3000):
+        logger.error('check grafana-server process failed')
+        if bflag:
+            return -3, "prometheus and grafana-server are not running"
+        return -2, "grafana-server is not running"
+
+    if bflag:
+        return -1, "prometheus is not running"
+
+    logger.info("health_check succeeded")
+    return 0,""
+
+def health_action():
+    logger.info("health_action")
+    ret = health_check()
+
+    cmd = ''
+    if ret == -1:
+        cmd = 'service prometheus start'
+    elif ret == -2:
+        cmd = 'service grafana-server start'
+    elif ret == -3:
+        cmd = 'service grafana-server start; service prometheus start'
+    else:
+        cmd = 'service grafana-server restart; service prometheus restart'
+    ret_code, output = exec_cmd(cmd)
+    if ret_code != 0:
+        logger.error('health_action start the service failed, reason %s' % output)
+        return -1
+
+    logger.info("health_action succeeded")
+    return 0
+
+def stop_server():
+    logger.info("stop_server")
+
+    if not check_local_service("prometheus", 9090):
+        logger.warning("stop_server skips the step to stop prometheus")
+    else:
+        ret_code, _ = exec_cmd('service prometheus stop')
+        if ret_code != 0:
+            logger.error('stop_server stop the service prometheus failed, reason %s' % output)
+            return -1
+
+    if not check_local_service("grafana-server", 3000):
+        logger.warning("stop_server skips the step to stop grafana-server")
+    else:
+        ret_code, _ = exec_cmd('service grafana-server stop')
+        if ret_code != 0:
+            logger.error('stop_server stop the service grafana-server failed, reason %s' % output)
+            return -1
+
+    logger.info("stop_server succeeded")
+    return 0
+
+def start_server():
+    logger.info("start_server")
+
+    if check_local_service("prometheus", 9090):
+        logger.warning("start_server skips the step to start prometheus")
+    else:
+        ret_code, _ = exec_cmd('service prometheus start')
+        if ret_code != 0:
+            logger.error('start_server start the service prometheus failed, reason %s' % output)
+            return -1
+
+    if check_local_service("grafana-server", 3000):
+        logger.warning("start_server skips the step to start grafana-server")
+    else:
+        ret_code, _ = exec_cmd('service grafana-server start')
+        if ret_code != 0:
+            logger.error('start_server start the service grafana-server failed, reason %s' % output)
+            return -1
+
+    logger.info("start_server succeeded")
+    return 0
+
+def restart_server():
+    logger.info("restart_server")
+    stop_server()
+    if 0 != start_server():
+        logger.error("restart_server failed")
+        return -1
+    logger.info("restart_server succeeded")
     return 0
 
 def print_usage():
@@ -197,6 +379,19 @@ if __name__ == "__main__":
             ret = modify_user(user_cnf)
         elif action == "dashboards":
             ret = disp_dashboard()
+        elif action == "deljob":
+            job_cnf = get_json_params(sys.argv[2:])
+            ret = del_job(job_cnf)
+        elif action == "healthcheck":
+            ret, _ = health_check()
+        elif action == "healthaction":
+            ret = health_action()
+        elif action == "start":
+            ret = start_server()
+        elif action == "stop":
+            ret = stop_server()
+        elif action == "restart":
+            ret = restart_server()
         exit(ret)
     except Exception, e:
         logger.error("%s" % traceback.format_exc())
